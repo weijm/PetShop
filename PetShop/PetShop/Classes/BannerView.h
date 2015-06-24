2a0e46a546506f120949976ade850b3a8c758a48
weijm <755528966@qq.com>
2015-06-24 09:53:49 +0800

    首页页面的修改

--- PetShop/PetShop/Classes/BannerView.h
+++ PetShop/PetShop/Classes/BannerView.h
@@ -15,11 +15,10 @@
     //banner上的标识页码的
     __weak IBOutlet UIPageControl *pageControl;
     
+    IBOutlet UIScrollView *bannerScrollView;
 }
 //加载图片和数据
 -(void)loadImageAndData:(NSArray*)dataArray;
-//适配scrollView的大小
--(void)adaperScrollview:(CGRect)frame;
 
 - (IBAction)leftOrRightAction:(id)sender;
 
